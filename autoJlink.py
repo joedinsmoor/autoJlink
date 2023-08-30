eb624a0dcd3dd407d1a513b30d36eedc28cad0ed
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-08-30 14:14:32 -0400

    Updated requirements, tests, and init framework

--- autoJlink.py
+++ autoJlink.py
@@ -1,3 +1,13 @@
-import pylink
 import re
 import csv
+from src.link import *
+
+modelNum = input("Input CPU Model Number: ")
+print(modelNum)
+
+speed = input("Please enter JTAG connection speed, if unknown, please hit enter: ")
+if speed == '':
+    speed = 4000
+
+print(info(modelNum, speed))
+
--- tests/tests.py
+++ tests/tests.py
@@ -1,2 +1,9 @@
 import pytest
 import pylink
+
+@pytest.fixture(scope="module")
+def autoJlinkTest():
+    #Testing here will only test to ensure that a given binary can be saved in a given output directory
+    #Due to the nature of how this library functions, and the function of JLink as a whole
+    #a full test suite is not possible. However, offline testing is being performed using a Segger JTrace Pro
+    f.open("testBinary.bin")
