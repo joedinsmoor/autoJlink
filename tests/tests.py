49c9acc91a5c5de81a8315ace5342e380a67038c
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-08-31 22:23:57 -0400

    fixed testing

--- tests/tests.py
+++ tests/tests.py
@@ -8,15 +8,20 @@ sys.path.append('./src')
 from link import *
 
 @pytest.fixture(scope="module")
-def autoJlinkTest(filename):
-    #Testing here will only test to ensure that a given binary can be saved in a given output directory
-    #Due to the nature of how this library functions, and the function of JLink as a whole
-    #a full test suite is not possible. However, offline testing is being performed using a Segger JTrace Pro
-    model = 'testCPU'
-    binary = 'AB-L18ER.bin'
-    return(output(model, binary))
+
+def copy_file(src_path, dest_path):
+    with open('AB-L18ER.bin', 'rb') as src_file:
+        with open('testCPU.bin', 'wb') as dest_file:
+            dest_file.write(src_file.read())
+
+def compare_files(file1, file2):
+    with open(file1, 'rb') as f1:
+        with open(file2, 'rb') as f2:
+            return f1.read() == f2.read()
 
 
-def autoJlinkAnswer(autoJlinkTest):
-    exists = autoJlinkTest('testCPU', 'AB-L18ER')
-    assert exists
+def autoJlinkAnswer(temp_files):
+    src_file, dest_file = temp_files
+
+    copy_file(src_file, dest_file)
+    assert compare_files(src_file, dest_file), "Copied contents are not the same."
