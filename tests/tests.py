a6e25d7c5085f24b6bd2f71b4472120edc9890cd
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-09-02 20:59:10 -0400

    fixed testing

--- tests/tests.py
+++ tests/tests.py
@@ -1,13 +1,6 @@
-import pytest
-import pylink
-from os.path import exists
 import os
-from pathlib import Path
-import sys
-sys.path.append('./src')
-from link import * #ignore errors here, the above line fixes this issue
-
-@pytest.fixture(scope="module")
+import tempfile
+import pytest
 
 def copy_file(src_path, dest_path):
     with open(src_path, 'rb') as src_file:
@@ -18,14 +11,28 @@ def compare_files(file1, file2):
     with open(file1, 'rb') as f1:
         with open(file2, 'rb') as f2:
             return f1.read() == f2.read()
-        
-def src_path():
-    return 'AB-L18ER'
 
-def dest_path():
-    return 'testCPU.bin'
+@pytest.fixture
+def temp_files(request):
+    src_content = b'This is the source file content.'
+    src_file = tempfile.NamedTemporaryFile(delete=False)
+    src_file.write(src_content)
+    src_file.close()
 
+    dest_file = tempfile.NamedTemporaryFile(delete=False)
+    dest_file.close()
 
-def testAutoJlink(copy_file, compare_files):
-    copy_file(src_path, dest_path)
-    assert compare_files(src_path, dest_path), "Copied contents are not the same."
+    def cleanup():
+        os.unlink(src_file.name)
+        os.unlink(dest_file.name)
+
+    request.addfinalizer(cleanup)
+
+    return src_file.name, dest_file.name
+
+def test_copy_and_compare(temp_files):
+    src_file, dest_file = temp_files
+
+    copy_file(src_file, dest_file)
+
+    assert compare_files(src_file, dest_file), "Copied contents are not the same."
